95b3279b5cf9f5e3124c4c5b19c61a447833a6d5
刘文杰 <liuwenjie123007@qq.com>
2022-01-26 23:28:49 +0900

    大规模并发：复制请求，速率限制，治愈异常的goroutine

---
+++ ch5/healing/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	var or func(channels ...<-chan interface{}) <-chan interface{}
+	or = func(channels ...<-chan interface{}) <-chan interface{} { // <1>
+		switch len(channels) {
+		case 0: // <2>
+			return nil
+		case 1: // <3>
+			return channels[0]
+		}
+
+		orDone := make(chan interface{})
+		go func() { // <4>
+			defer close(orDone)
+
+			switch len(channels) {
+			case 2: // <5>
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default: // <6>
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...): // <6>
+				}
+			}
+		}()
+		return orDone
+	}
+
+	type startGoroutineFn func(
+		done <-chan interface{},
+		pulseInterval time.Duration,
+	) (heartbeat <-chan interface{})
+
+	newSteward := func(
+		timeout time.Duration,
+		startGoroutine startGoroutineFn,
+	) startGoroutineFn {
+		return func(done <-chan interface{},
+			pulseInterval time.Duration,
+		) <-chan interface{} {
+			heartbeat := make(chan interface{})
+			go func() {
+				defer close(heartbeat)
+
+				var wardDone chan interface{}
+				var wardHeartbeat <-chan interface{}
+				startWard := func() {
+					wardDone = make(chan interface{})
+					wardHeartbeat = startGoroutine(or(wardDone, done), timeout/2)
+				}
+				startWard()
+				pulse := time.Tick(pulseInterval)
+
+			monitorLoop:
+				for {
+					timeoutSignal := time.After(timeout)
+					for {
+						select {
+						case <-pulse:
+							select {
+							case heartbeat <- struct{}{}:
+							default:
+							}
+						case <-wardHeartbeat:
+							continue monitorLoop
+						case <-timeoutSignal:
+							log.Println("steward: ward unhealthy; restarting")
+							close(wardDone)
+							startWard()
+							continue monitorLoop
+						case <-done:
+							return
+						}
+					}
+				}
+			}()
+			return heartbeat
+		}
+	}
+
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	doWork := func(done <-chan interface{}, _ time.Duration) <-chan interface{} {
+		log.Println("ward: Hello, I'm irresponsible!")
+		go func() {
+			<-done
+			log.Println("ward: i am halting.")
+		}()
+		return nil
+	}
+	doWorkWithSteward := newSteward(4*time.Second, doWork)
+
+	done := make(chan interface{})
+	time.AfterFunc(9*time.Second, func() {
+		log.Println("maan: halting steward and war")
+		close(done)
+	})
+
+	for range doWorkWithSteward(done, 4) {
+	}
+	log.Println("Done")
+}
---
+++ ch5/speed-limit/network-disklimit/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"golang.org/x/time/rate"
+	"log"
+	"os"
+	"sort"
+	"sync"
+	"time"
+)
+
+type RateLimiter interface {
+	Wait(context.Context) error
+	Limit() rate.Limit
+}
+
+func MultiLimiter(limiters ...RateLimiter) *multiLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+	sort.Slice(limiters, byLimit)
+	return &multiLimiter{limiters: limiters}
+}
+
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func (l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
+
+func Per(eventCount int, duration time.Duration) rate.Limit {
+	return rate.Every(duration / time.Duration(eventCount))
+}
+
+type APIConnection struct {
+	networkLimit,
+	diskLimit,
+	apiLimit RateLimiter
+}
+
+func Open() *APIConnection {
+	secondLimit := rate.NewLimiter(Per(2, time.Second), 1)
+	minuteLimit := rate.NewLimiter(Per(10, time.Minute), 10)
+	return &APIConnection{
+		apiLimit:     MultiLimiter(secondLimit, minuteLimit),
+		diskLimit:    MultiLimiter(rate.NewLimiter(rate.Limit(1), 1)),
+		networkLimit: MultiLimiter(rate.NewLimiter(Per(3, time.Second), 3)),
+	}
+}
+
+func (a *APIConnection) ReadFile(ctx context.Context) error {
+	if err := MultiLimiter(a.apiLimit, a.diskLimit).Wait(ctx); err != nil {
+		return err
+	}
+	// 假设我们在这里执行一些逻辑
+	return nil
+}
+
+func (a *APIConnection) ResolveAddress(ctx context.Context) error {
+	if err := MultiLimiter(a.apiLimit, a.networkLimit).Wait(ctx); err != nil {
+		return err
+	}
+	//  假设我们在这里执行一些逻辑
+	return nil
+}
+
+func main() {
+	defer log.Printf("Done.")
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	apiConnection := Open()
+	var wg sync.WaitGroup
+	wg.Add(20)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := apiConnection.ReadFile(context.Background())
+			if err != nil {
+				log.Printf("cannot ReadFile: %v", err)
+			}
+			log.Printf("ReadFile")
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := apiConnection.ResolveAddress(context.Background())
+			if err != nil {
+				log.Printf("cannot ResolveAddress: %v", err)
+			}
+			log.Printf("ResolveAddress")
+		}()
+	}
+
+	wg.Wait()
+}
